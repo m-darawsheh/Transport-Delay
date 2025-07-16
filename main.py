2dda7af2ad60b5261c3cdeeffe57906747eff177
m-darawsheh <mohamaddarawsha2005@gmail.com>
2025-07-16 12:12:05 +0300

    Add .gitignore, create get_info route and template, remove unused test file

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/get_info')
+def get_info():
+    return render_template('get_info.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
